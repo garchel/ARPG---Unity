23e8b8f51ce91df013629114d4b3acdbc8601a6a
Garchel <paulovictor.co97@gmail.com>
2024-07-09 22:00:07 -0300

    Video 7 - UI Manager

---
+++ UI/UIPanelManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPanelManager : MonoBehaviour
+{
+    [SerializeField] GameObject inventoryPanel;
+    [SerializeField] GameObject questsPanel;
+    [SerializeField] GameObject statsPanel;
+
+    private void Awake()
+    {
+        inventoryPanel.SetActive(false);
+        questsPanel.SetActive(false);
+        statsPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            OpenInventory();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            OpenQuests();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            OpenStats();
+        }
+    }
+
+    public void OpenInventory()
+    {
+        inventoryPanel.SetActive(!inventoryPanel.activeInHierarchy);
+
+    }
+
+    public void OpenQuests()
+    {
+        questsPanel.SetActive(!questsPanel.activeInHierarchy);
+        statsPanel.SetActive(false);
+
+    }
+
+    public void OpenStats()
+    {
+        statsPanel.SetActive(!statsPanel.activeInHierarchy);
+        questsPanel.SetActive(false);
+
+    }
+}
