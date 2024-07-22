937a83e087fdbd6c2d007ce00836bfa885a04392
Garchel <paulovictor.co97@gmail.com>
2024-07-22 17:57:54 -0300

    Video 12 - Inventory pt1

---
+++ UI/InventoryPanel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InventoryPanel : MonoBehaviour
+{
+    [SerializeField] TMPro.TextMeshProUGUI currencyText;
+    [SerializeField] Inventory playerInventory;
+    int currency = -1;
+
+    void Update()
+    {
+        if (currency != playerInventory.currency)
+        {
+            currencyText.text = playerInventory.currency.ToString();
+            currency = playerInventory.currency;
+
+        }
+    }
+
+}
